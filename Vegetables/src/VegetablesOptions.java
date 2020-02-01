f04d75ffccac8fd8340f897090f9d68529c8d863
SydneyCaulfeild <sydney.ec6@gmail.com>
2020-02-01 15:51:31 -0500

    changed vars to public and printed info

--- Vegetables/src/VegetablesOptions.java
+++ Vegetables/src/VegetablesOptions.java
@@ -1,21 +1,23 @@
 
 public class VegetablesOptions {
 
-	private double vitaminA;
-	private double vitaminC;
-	private double vitaminK;
-	private double iron;
-	private double calcium;
-	private double calories;
-	private double potassium;
+	public double vitaminA;
+	public double vitaminC;
+	public double vitaminK;
+	public double iron;
+	public double calcium;
+	public double calories;
+	public double potassium;
 	
-	private boolean singleUse;
-	private int lifeCycle;
-	private double dailyWater;
-	private double area;
-	private double footprint;
+	public boolean singleUse;
+	public int lifeCycle;
+	public double dailyWater;
+	public double area;
+	public double footprint;
+	public String name;
 	
-	public VegetablesOptions (double vitaminA,
+	public VegetablesOptions (String name,
+			double vitaminA,
 			double vitaminC,
 			double vitaminK,
 			double iron,
@@ -29,6 +31,7 @@ public class VegetablesOptions {
 			double area,
 			double footprint) {
 		
+		this.name = name;
 		this.vitaminA = vitaminA;
 		this.vitaminC = vitaminC;
 		this.vitaminK = vitaminK;
@@ -44,12 +47,25 @@ public class VegetablesOptions {
 		
 	}
 	public static void main(String[] args){
-		 VegetablesOptions beet = new VegetablesOptions(0.0, 0.00612,0.00000034,0.004,0.00272,1.6,43.0, 0.05186,true, 55, 21.77,  60.0,  2.1);
-		 VegetablesOptions carrots = new VegetablesOptions(0.000509, 0.0036, 0.000008052, 0.000183, 0.021013, 0.45, 20.00, 0.01952, true, 75, 21.05, 58, 0.4);
-		 VegetablesOptions kale = new VegetablesOptions(0.0017, 0.06955, 0.00138, 0.00342, 0.18724, 5.187, 76.44, 0.00062244, true, 65, 29.48, 65, 0.4);
-		 VegetablesOptions spinach = new VegetablesOptions(0.0017553, 0.01753, 0.00030133, 0.001691, 0.06178, 1.785, 14.4, 0.3482, true, 49, 5.90, 13, 0.5);
-		 VegetablesOptions tomato = new VegetablesOptions( 0.00002372, 0.0078, 0.0000045, 0.000155, 0.005962, 0.5, 10.1269,  0.135, false, 10, 421.37, 929, 2.0);
+		 VegetablesOptions beet = new VegetablesOptions("beet",0.0, 0.00612,0.00000034,0.004,0.00272,1.6,43.0, 0.05186,true, 55, 21.77,  60.0,  2.1);
+		 VegetablesOptions carrots = new VegetablesOptions("carrots",0.000509, 0.0036, 0.000008052, 0.000183, 0.021013, 0.45, 20.00, 0.01952, true, 75, 21.05, 58, 0.4);
+		 VegetablesOptions kale = new VegetablesOptions("kale",0.0017, 0.06955, 0.00138, 0.00342, 0.18724, 5.187, 76.44, 0.00062244, true, 65, 29.48, 65, 0.4);
+		 VegetablesOptions spinach = new VegetablesOptions("spinach",0.0017553, 0.01753, 0.00030133, 0.001691, 0.06178, 1.785, 14.4, 0.3482, true, 49, 5.90, 13, 0.5);
+		 VegetablesOptions tomato = new VegetablesOptions("tomato", 0.00002372, 0.0078, 0.0000045, 0.000155, 0.005962, 0.5, 10.1269,  0.135, false, 10, 421.37, 929, 2.0);
 
+	}
+	
+	public String toString(VegetablesOptions veggie) {
+		String oneTime;
+		if (veggie.singleUse == true) {
+			oneTime = "can";
+		}
+		else {
+			oneTime = "can not";
+		}
+		System.out.println("Your "+veggie.name + " "+ oneTime+"continue to produce yield after it has been harvested.");
+		System.out.println("It can be harvested in "+veggie.lifeCycle+"days.");
+		System.out.println("It's carbon footprint is "+veggie.footprint+"kg of CO2 per kg of vegetable.");
 	}
 	
 }
